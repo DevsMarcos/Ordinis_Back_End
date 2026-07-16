01074e396a069b0fecca84d6cce9670267651334
Marcos Pedro de Macedo <marcospedromdmacedo@gmail.com>
2026-07-15 21:35:38 -0300

    Documentacao das classes de infra

--- src/main/java/os/system/SistemaOrdemServico/infrastructure/out/persistence/OrdemMapper.java
+++ src/main/java/os/system/SistemaOrdemServico/infrastructure/out/persistence/OrdemMapper.java
@@ -3,8 +3,15 @@ package os.system.SistemaOrdemServico.infrastructure.out.persistence;
 import os.system.SistemaOrdemServico.Domain.Entities.Ordem;
 import os.system.SistemaOrdemServico.application.DTOs.OrdemDTO;
 
+
+/**
+ * A função de um mapper numa alicação é realizar o mapeamento/conversão de um Ojbeto de Transferencia para uma Entidade de Domínio
+ * ou até mesmo uma Entidade de Banco de dados. Isso é realizado para garantir a segurança da aplicação,
+ * evitando expor tabelas sensiveis
+ * */
 public class OrdemMapper {
 
+    //Converte a o dominio para a entidade JPA
     public static OrdemJpaEntity paraJpaEntity(Ordem ordem) {
         OrdemJpaEntity e = new OrdemJpaEntity();
         e.setId(ordem.getId());
@@ -20,10 +27,12 @@ public class OrdemMapper {
         return e;
     }
 
+    //Converte a JPA para domínio
     public static Ordem paraDomain(OrdemJpaEntity e) {
         return Ordem.reconstituir(e.getId(), e.getStatus(), e.getNomeDoCliente(), e.getTelefone(), e.getProduto(), e.getMarca(), e.getModelo(), e.getCaracteristicaProduto(), e.getDataDeAbertura(), e.getDataFechamento());
     }
 
+    //COnverte o domínio para o DTO
     public static OrdemDTO paraDTO(Ordem ordem) {
         return new OrdemDTO(ordem.getNomeDoCliente(), ordem.getTelefone(), ordem.getProduto(), ordem.getMarca(), ordem.getModelo(), ordem.getCaracteristicaProduto());
     }
--- src/main/java/os/system/SistemaOrdemServico/infrastructure/out/persistence/OrdemRepositoryImpl.java
+++ src/main/java/os/system/SistemaOrdemServico/infrastructure/out/persistence/OrdemRepositoryImpl.java
@@ -7,14 +7,26 @@ import os.system.SistemaOrdemServico.Domain.Repositories.OrdemRepository;
 
 import java.util.Optional;
 
+/**
+ * Esta classe realiza a implementação da Interface do OrdemRepository
+ * Desta forma a mesma é obrigada a implementar as funções básicas de
+ * iteração com o banco de dados, definida na interface
+ * */
+
 @Repository
 public class OrdemRepositoryImpl implements OrdemRepository {
     private final OrdemJpaRepository jpaRepository;
 
+    /**Aqui é realizo a injeção da dependência do OrdemJpaRepository que extende a JPA do spring e fornece
+    as funções nativas de manipulação do banco*/
     public OrdemRepositoryImpl( OrdemJpaRepository jpaRepository) {
         this.jpaRepository = jpaRepository;
     }
 
+    /**Função implementada que salva uma ordem
+     * Ela recebe uma entidade do domínio, converte a mesma para a entidade JPA
+     * e salva a mesma no banco de dados, retornando um DTO para o front
+     * */
     @Override
     public Ordem salvar(Ordem ordem) {
         OrdemJpaEntity entity = OrdemMapper.paraJpaEntity(ordem);
@@ -22,6 +34,7 @@ public class OrdemRepositoryImpl implements OrdemRepository {
         return OrdemMapper.paraDomain(salva);
     }
 
+    /**Não implementado ainda*/
     @Override
     public Optional<Ordem> buscarPorId(Long id) {
         return Optional.empty();
