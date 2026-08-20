39a5346754b405532f57496105f29c2978c04771
Marcos Pedro de Macêdo <marcospedromdmacedo@outlook.com>
2026-08-20 20:06:23 -0300

    teste de CI

--- src/main/java/os/system/SistemaOrdemServico/SistemaOrdemServicoApplication.java
+++ src/main/java/os/system/SistemaOrdemServico/SistemaOrdemServicoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SistemaOrdemServicoApplication {
+	//teste de commit
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaOrdemServicoApplication.class, args);
