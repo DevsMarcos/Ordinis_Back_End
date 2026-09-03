9a562ecfe1f613670f72e2a1c2237b780fc5bb10
Marcos Pedro de Macêdo <marcospmcedo@gmail.com>
2026-09-03 07:24:58 -0300

    teste CI

--- src/main/java/os/system/SistemaOrdemServico/SistemaOrdemServicoApplication.java
+++ src/main/java/os/system/SistemaOrdemServico/SistemaOrdemServicoApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SistemaOrdemServicoApplication {
-	//teste de commit e CD
-
+//Teste com deploy na AWS
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaOrdemServicoApplication.class, args);
 	}
